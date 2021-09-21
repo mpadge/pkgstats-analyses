d359896906af2030002a50ee515f6ab34c309a00
mpadge <mark.padgham@email.com>
2021-09-21 12:02:59 +0200

    add analyses-submission-rates

---
+++ R/analyses-submission-rates.R
@@ -0,0 +1,26 @@
+
+#' Analyse submission rates of packages
+#'
+#' @param datafile Path to local `pkgstats` data
+#' @export
+pkgstats_analyse_submission_rates <- function (datafile) {
+
+    x <- load_pkgstats_data (datafile, raw = TRUE, latest = FALSE) |>
+        transform_pkgstats_data ()
+
+    m_analyse_submission_rates (x)
+}
+
+analyse_submission_rates <- function (x) {
+
+    x |>
+        dplyr::arrange (date) |>
+        dplyr::group_by (package) |>
+        dplyr::summarise (n = length (package),
+                          time_med = median (diff (date)),
+                          time_mn = mean (diff (date)),
+                          time_rate = ifelse (length (date) <= 2L,
+                                              NA,
+            unname (lm (diff (date) ~ seq_along (date) [-1])$coefficients [2])))
+}
+m_analyse_submission_rates <- memoise::memoise (analyse_submission_rates)
